25d00b41bd1a49fdd99b35089695d79e135345fb
chocolaggibbiddori <didrkd684@naver.com>
2022-11-26 22:14:53 +0900

    feat : Board 좌표값 수정

--- src/board/Board.java
+++ src/board/Board.java
@@ -19,8 +19,19 @@ public class Board {
     public static final int G = 6;
     public static final int H = 7;
     public static final int I = 8;
+    public static final int J = 9;
 
-    public Unit[][] boardArray = new Unit[BOARD_WIDTH][BOARD_HEIGHT];
+    public static final int K = 0;
+    public static final int L = 1;
+    public static final int M = 2;
+    public static final int N = 3;
+    public static final int O = 4;
+    public static final int P = 5;
+    public static final int Q = 6;
+    public static final int R = 7;
+    public static final int S = 8;
+
+    public Unit[][] boardArray = new Unit[BOARD_HEIGHT][BOARD_WIDTH];
 
     private Board() {}
 
