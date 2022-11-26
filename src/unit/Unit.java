ea0bee1ade85cf409af97db8140f74166ef160be
chocolaggibbiddori <didrkd684@naver.com>
2022-11-26 17:01:07 +0900

    style : 정리

--- src/unit/Unit.java
+++ src/unit/Unit.java
@@ -17,31 +17,24 @@ public abstract class Unit {
     public String getTeamName() {
         return teamName;
     }
-
     public void setTeamName(String teamName) {
         this.teamName = teamName;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public int getPositionX() {
         return positionX;
     }
-
     public void setPositionX(int positionX) {
         this.positionX = positionX;
     }
-
     public int getPositionY() {
         return positionY;
     }
-
     public void setPositionY(int positionY) {
         this.positionY = positionY;
     }
