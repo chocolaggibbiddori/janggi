1cfb31a19b3ad7eb1965d22d34155464608843c3
chocolaggibbiddori <didrkd684@naver.com>
2022-11-26 23:15:26 +0900

    feat : 졸병 추가

---
+++ src/unit/Soldier.java
@@ -0,0 +1,80 @@
+package unit;
+
+import board.Board;
+
+public class Soldier extends Unit {
+
+    boolean isGreen = getTeamName().equals(Board.TEAM_GREEN);
+    int forwardDirect = isGreen ? -1 : 1;
+
+    public Soldier(int positionX, int positionY, String teamName) {
+        super("soldier", positionX, positionY, teamName, 2);
+    }
+
+    @Override
+    public boolean move(int moveToX, int moveToY) {
+        int positionX = getPositionX();
+        int positionY = getPositionY();
+
+        if (isPossible(moveToX, moveToY, positionX, positionY)) {
+            setPositionX(moveToX);
+            setPositionY(moveToY);
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isPossible(int moveToX, int moveToY, int positionX, int positionY) {
+        if (isAbleToMoveForward(moveToX, moveToY, positionX, positionY)) {
+            return true;
+        } else if (isAbleToMoveSide(moveToX, moveToY, positionX, positionY)) {
+            return true;
+        } else if (isAbleToMoveDiagonal(moveToX, moveToY, positionX, positionY)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isAbleToMoveForward(int moveToX, int moveToY, int positionX, int positionY) {
+        return positionX + forwardDirect == moveToX && positionY == moveToY;
+    }
+
+    private boolean isAbleToMoveSide(int moveToX, int moveToY, int positionX, int positionY) {
+        return positionX == moveToX && (positionY - 1 == moveToY || positionY + 1 == moveToY);
+    }
+
+    private boolean isAbleToMoveDiagonal(int moveToX, int moveToY, int positionX, int positionY) {
+        if (!isInPalace(positionX, positionY)) {
+            return false;
+        }
+        if (!isInPalace(moveToX, moveToY)) {
+            return false;
+        }
+
+        if (isGreen) {
+            if (positionX == Board.C && moveToX == Board.B && moveToY == Board.O) {
+                return true;
+            } else if (positionX == Board.B && positionY == Board.O && moveToX == Board.A) {
+                return true;
+            }
+        } else {
+            if (positionX == Board.H && moveToX == Board.I && moveToY == Board.O) {
+                return true;
+            } else if (positionX == Board.I && positionY == Board.O && moveToX == Board.J) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean isInPalace(int positionX, int positionY) {
+        if (isGreen) {
+            return (positionX >= Board.A && positionX <= Board.C) && (positionY >= Board.N && positionY <= Board.P);
+        } else {
+            return (positionX >= Board.H && positionX <= Board.J) && (positionY >= Board.N && positionY <= Board.P);
+        }
+    }
+}
