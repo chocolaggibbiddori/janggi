dab7f64f5cbf69a925a9a807fccff0a47068a408
chocolaggibbiddori <didrkd684@naver.com>
2022-11-26 16:49:20 +0900

    feat : 장기알 부모 클래스 선언

---
+++ src/unit/Unit.java
@@ -0,0 +1,35 @@
+package unit;
+
+public abstract class Unit {
+
+    private String name;
+    private int positionX;
+    private int positionY;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
+
+    public abstract void move();
+    public abstract void remove();
+}
