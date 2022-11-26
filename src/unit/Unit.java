b62bc4b2917a50cdbd7e0e1d737cc8ebc21b2539
chocolaggibbiddori <didrkd684@naver.com>
2022-11-26 21:25:21 +0900

    feat : Unit - move 조건식 변경

--- src/unit/Unit.java
+++ src/unit/Unit.java
@@ -43,9 +43,9 @@ public abstract class Unit {
     public abstract boolean move(int moveToX, int moveToY);
 
     public boolean isInBoard(int moveToX, int moveToY) {
-        if (moveToX < 0 || moveToX > Board.BOARD_WIDTH) {
+        if (moveToX < 0 || moveToX >= Board.BOARD_WIDTH) {
             return false;
-        } else if (moveToY < 0 || moveToY > Board.BOARD_HEIGHT) {
+        } else if (moveToY < 0 || moveToY >= Board.BOARD_HEIGHT) {
             return false;
         }
 
