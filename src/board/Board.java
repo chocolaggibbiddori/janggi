e9c423370a0e189c31b20ead16e1ce48e88fbbda
chocolaggibbiddori <didrkd684@naver.com>
2022-11-26 17:22:15 +0900

    feat : Board - 가로, 세로 변수 public으로 변경

--- src/board/Board.java
+++ src/board/Board.java
@@ -2,6 +2,6 @@ package board;
 
 public class Board {
 
-    private static final int BOARD_WIDTH = 9;
-    private static final int BOARD_HEIGHT = 10;
+    public static final int BOARD_WIDTH = 9;
+    public static final int BOARD_HEIGHT = 10;
 }
