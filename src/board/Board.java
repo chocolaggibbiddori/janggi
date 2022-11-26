0d6da70ff8608c0642b3484b83560a42fc0f944f
chocolaggibbiddori <didrkd684@naver.com>
2022-11-26 21:30:07 +0900

    feat : 팀명 상수 추가

--- src/board/Board.java
+++ src/board/Board.java
@@ -4,6 +4,8 @@ public class Board {
 
     public static final int BOARD_WIDTH = 9;
     public static final int BOARD_HEIGHT = 10;
+    public static final String TEAM_GREEN = "Green";
+    public static final String TEAM_RED = "Red";
 
     public static final int A = 0;
     public static final int B = 1;
