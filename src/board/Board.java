2ae4db920b1d694d65c4957749ca5c4c759258fa
chocolaggibbiddori <didrkd684@naver.com>
2022-11-26 21:48:07 +0900

    feat : Board 배열 추가

--- src/board/Board.java
+++ src/board/Board.java
@@ -1,12 +1,15 @@
 package board;
 
+import unit.Unit;
+
 public class Board {
+
+    private static Board board = null;
 
     public static final int BOARD_WIDTH = 9;
     public static final int BOARD_HEIGHT = 10;
     public static final String TEAM_GREEN = "Green";
     public static final String TEAM_RED = "Red";
-
     public static final int A = 0;
     public static final int B = 1;
     public static final int C = 2;
@@ -16,4 +19,16 @@ public class Board {
     public static final int G = 6;
     public static final int H = 7;
     public static final int I = 8;
+
+    public Unit[][] boardArray = new Unit[BOARD_WIDTH][BOARD_HEIGHT];
+
+    private Board() {}
+
+    public Board getInstance() {
+        if (board == null) {
+            board = new Board();
+        }
+
+        return board;
+    }
 }
