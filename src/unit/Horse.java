5b07a5e71e01032b43cf785b37dcc9417158735c
chocolaggibbiddori <didrkd684@naver.com>
2022-11-27 23:27:04 +0900

    feat : Horse - 생성

---
+++ src/unit/Horse.java
@@ -0,0 +1,50 @@
+package unit;
+
+import board.Board;
+import rule.Score;
+
+import static board.Board.boardArray;
+
+public class Horse extends Unit {
+
+    public Horse(int positionX, int positionY, String teamName) {
+        super("horse", positionX, positionY, teamName, Score.HORSE_SCORE);
+    }
+
+    @Override
+    protected boolean isPossible(int moveToX, int moveToY) {
+        //좌표값 차이가 3이면
+        int xDiff = Math.abs(moveToX - positionX);
+        int yDiff = Math.abs(moveToY - positionY);
+
+        if (xDiff + yDiff == 3) {
+            //x값이나 y값이 같으면 false
+            if (moveToX == positionX || moveToY == positionY) {
+                return false;
+            }
+
+            //멱이 막히면 false
+            return !isBlocked(moveToX, moveToY);
+        }
+
+        return false;
+    }
+
+    private boolean isBlocked(int moveToX, int moveToY) {
+        Board board = Board.getInstance();
+        int xDiff = positionX - moveToX;
+        int yDiff = positionY - moveToY;
+
+        if (xDiff == 2) {
+            return boardArray[positionX + 1][positionY] != null;
+        } else if (xDiff == -2) {
+            return boardArray[positionX - 1][positionY] != null;
+        } else if (yDiff == 2) {
+            return boardArray[positionX][positionY + 1] != null;
+        } else if (yDiff == -2) {
+            return boardArray[positionX][positionY - 1] != null;
+        }
+
+        return false;
+    }
+}
