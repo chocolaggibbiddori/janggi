1b2b589d5eb51ab4ef1f62766fe552b6b8e8c3d5
chocolaggibbiddori <didrkd684@naver.com>
2022-11-27 21:05:32 +0900

    feat : Scholar(사) 추가

---
+++ src/unit/Scholar.java
@@ -0,0 +1,61 @@
+package unit;
+
+import board.Board;
+import rule.Score;
+
+public class Scholar extends Unit{
+
+    boolean isGreen = getTeamName().equals(Board.TEAM_GREEN);
+
+    public Scholar(int positionX, int positionY, String teamName) {
+        super("scholar", positionX, positionY, teamName, Score.SCHOLAR_SCORE);
+    }
+
+    @Override
+    protected boolean isPossible(int moveToX, int moveToY, int positionX, int positionY) {
+        if (isAbleToMoveNEWS(moveToX, moveToY, positionX, positionY)) {
+            return true;
+        } else if (isAbleToMoveDiagonal(moveToX, moveToY, positionX, positionY)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isAbleToMoveNEWS(int moveToX, int moveToY, int positionX, int positionY) {
+        if (moveToX == positionX) {
+            return Math.abs(moveToY - positionY) == 1;
+        } else if (moveToY == positionY) {
+            return Math.abs(moveToX - positionX) == 1;
+        }
+
+        return false;
+    }
+
+    private boolean isAbleToMoveDiagonal(int moveToX, int moveToY, int positionX, int positionY) {
+        if (!isInPalace(positionX, positionY)) {
+            return false;
+        }
+        if (!isInPalace(moveToX, moveToY)) {
+            return false;
+        }
+
+        return isCenterOfPalace(moveToX, moveToY) || isCenterOfPalace(positionX, positionY);
+    }
+
+    private boolean isInPalace(int positionX, int positionY) {
+        if (isGreen) {
+            return (positionX >= Board.A && positionX <= Board.C) && (positionY >= Board.N && positionY <= Board.P);
+        } else {
+            return (positionX >= Board.H && positionX <= Board.J) && (positionY >= Board.N && positionY <= Board.P);
+        }
+    }
+
+    private boolean isCenterOfPalace(int positionX, int positionY) {
+        if (isGreen) {
+            return positionX == Board.I && positionY == Board.O;
+        } else {
+            return positionX == Board.B && positionY == Board.O;
+        }
+    }
+}
