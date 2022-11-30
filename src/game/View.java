b1163c2333bb44632a5110289f5cd28f4eaec148
chocolaggibbiddori <didrkd684@naver.com>
2022-11-30 17:27:33 +0900

    feat : View - drawBoard 완성

--- src/game/View.java
+++ src/game/View.java
@@ -12,6 +12,14 @@ public class View {
         int rowLength = units.length;
         int colLength = units[0].length;
 
+        for (int i = 0; i < colLength; i++) {
+            System.out.print("    ");
+            System.out.print(boardColArray[i]);
+            System.out.print("   ");
+        }
+        System.out.println();
+        System.out.println();
+
         for (int i = 0; i < rowLength; i++) {
             System.out.print(boardRowArray[i] + "  ");
             for (int j = 0; j < colLength; j++) {
@@ -22,14 +30,49 @@ public class View {
                 }
                 if (j != colLength - 1) {
                     System.out.print("----");
+                } else {
+                    System.out.println("  " + boardRowArray[i]);
                 }
             }
-            System.out.println();
             if (i != rowLength - 1) {
                 for (int num = 1; num <= 2; num++) {
                     for (int c = 0; c < colLength; c++) {
                         if (c != colLength - 1) {
-                            System.out.print("    |   ");
+                            if (i == 0 || i == 7) {
+                                if (num == 1) {
+                                    if (c == 3) {
+                                        System.out.print("    |  \\");
+                                    } else if (c == 5) {
+                                        System.out.print(" /  |   ");
+                                    } else {
+                                        System.out.print("    |   ");
+                                    }
+                                } else {
+                                    if (c == 4) {
+                                        System.out.print(" \\  |  /");
+                                    } else {
+                                        System.out.print("    |   ");
+                                    }
+                                }
+                            } else if (i == 1 || i == 8) {
+                                if (num == 1) {
+                                    if (c == 4) {
+                                        System.out.print(" /  |  \\");
+                                    } else {
+                                        System.out.print("    |   ");
+                                    }
+                                } else {
+                                    if (c == 3) {
+                                        System.out.print("    |  /");
+                                    } else if (c == 5) {
+                                        System.out.print(" \\  |   ");
+                                    } else {
+                                        System.out.print("    |   ");
+                                    }
+                                }
+                            } else {
+                                System.out.print("    |   ");
+                            }
                         } else {
                             System.out.println("    |  ");
                         }
@@ -38,11 +81,18 @@ public class View {
             }
         }
         System.out.println();
+
         for (int i = 0; i < colLength; i++) {
             System.out.print("    ");
             System.out.print(boardColArray[i]);
             System.out.print("   ");
         }
+        System.out.println();
+    }
+
+    public void drawScore(int greenScore, double redScore) {
+        System.out.println("초나라 점수: " + greenScore);
+        System.out.println("한나라 점수: " + redScore);
         System.out.println();
     }
 }
